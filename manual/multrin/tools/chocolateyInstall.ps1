dc66654792ee8900edfdbf11df63f9e8091d8282
Steven Kraft <steven.kraftjp@gmail.com>
2020-04-09 12:46:32 -0400

    Updated Multrin to 1.2.2

--- manual/multrin/tools/chocolateyInstall.ps1
+++ manual/multrin/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'multrin'
   fileType               = 'exe'
-  url                    = 'https://github.com/sentialx/multrin/releases/download/v1.0.0/Multrin-Setup-1.0.0.exe'
-  checksum               = '675F70470557C1881B490CA9A019D5DC'
-  checksumType           = 'md5'
+  url                    = 'https://github.com/sentialx/multrin/releases/download/v1.2.2/Multrin-Setup-1.2.2.exe'
+  checksum               = 'b5e75152e5e60f2740996532979b1288626557aa3cd564cea898c2b3bc320f2f'
+  checksumType           = 'sha256'
   silentArgs             = '/S /L=$LCID'
 }
 Install-ChocolateyPackage @packageArgs
